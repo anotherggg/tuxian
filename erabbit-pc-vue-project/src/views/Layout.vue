0c152174b5aed7358e5a840f2a60ab37eb9bc77e
unknown <772172914@qq.com>
2022-12-12 16:28:08 +0800

    首页-底部布局

--- erabbit-pc-vue-project/src/views/Layout.vue
+++ erabbit-pc-vue-project/src/views/Layout.vue
@@ -6,15 +6,16 @@
       <!--二级路由-->
       <router-view></router-view>
     </main>
-    <footer>底部</footer>
+    <AppFooter />
   </div>
 </template>
 <script>
 import AppTopnav from '@/components/app-topnav';
 import AppHeader from '@/components/app-header';
+import AppFooter from '@/components/app-footer';
 export default {
   name: 'xtx-layout',
-  components: { AppTopnav, AppHeader },
+  components: { AppTopnav, AppHeader, AppFooter },
 };
 </script>
 
