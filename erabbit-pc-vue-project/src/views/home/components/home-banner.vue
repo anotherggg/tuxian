861d8bc8bd8fc91b9b6882db0b2fcbbac3d4782c
unknown <772172914@qq.com>
2022-12-15 16:06:13 +0800

    首页主体-轮播图-渲染结构

--- erabbit-pc-vue-project/src/views/home/components/home-banner.vue
+++ erabbit-pc-vue-project/src/views/home/components/home-banner.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home-banner">
-    <XtxCarousel />
+    <XtxCarousel :sliders="sliders" />
   </div>
 </template>
 <script>
+import { findBanner } from '@/api/home.js';
+import { ref } from 'vue';
 export default {
   name: 'HomeBanner',
+  setup() {
+    const sliders = ref([]);
+    findBanner().then((data) => {
+      sliders.value = data.result;
+    });
+    return { sliders };
+  },
 };
 </script>
 <style scoped lang="less">
