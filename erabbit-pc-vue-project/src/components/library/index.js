00339f9a262bf8a1f1fb217fe3407bc39cd040cd
unknown <772172914@qq.com>
2023-01-05 09:42:50 +0800

    顶级类目-面包屑组件-初级

--- erabbit-pc-vue-project/src/components/library/index.js
+++ erabbit-pc-vue-project/src/components/library/index.js
@@ -5,6 +5,7 @@
 import XtxSkeleton from './xtx-skeleton.vue';
 import XtxCarousel from './xtx-carousel.vue';
 import XtxMore from './xtx-more.vue';
+import XtxBread from './xtx-bread.vue';
 import defaultImg from '@/assets/images/200.png';
 
 export default {
@@ -14,6 +15,7 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton);
     app.component(XtxCarousel.name, XtxCarousel);
     app.component(XtxMore.name, XtxMore);
+    app.component(XtxBread.name, XtxBread);
     defineDirective(app);
   },
 };
