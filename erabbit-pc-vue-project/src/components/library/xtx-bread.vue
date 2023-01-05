df8f0ee68e192e2db204866aba4791eee31bceb8
unknown <772172914@qq.com>
2023-01-05 13:53:48 +0800

    顶级类目-面包屑组件-高级

--- erabbit-pc-vue-project/src/components/library/xtx-bread.vue
+++ erabbit-pc-vue-project/src/components/library/xtx-bread.vue
@@ -18,6 +18,7 @@
 </template>
 
 <script>
+import { h } from 'vue';
 export default {
   name: 'XtxBread',
   props: {
@@ -30,13 +31,30 @@ export default {
       default: '',
     },
   },
+  render() {
+    // 获取默认插槽的node结构，按照要求拼接结构
+    const items = this.$slots.default();
+    const dymanicItems = [];
+    items.forEach((item, i) => {
+      dymanicItems.push(item);
+      if (i < items.length - 1) {
+        dymanicItems.push(h('i', { class: 'iconfont icon-angle-right' }));
+      }
+    });
+    return h('div', { class: 'xtx-bread' }, dymanicItems);
+  },
 };
 </script>
 
-<style scoped lang="less">
+<style lang="less">
+// 去除 scoped 属性，目的：然样式作用到xtx-bread-item组件
 .xtx-bread {
   display: flex;
   padding: 25px 10px;
+  // ul li:last-child {}
+  // 先找到父元素，找到所有的子元素，找到最后一个，判断是不是LI，是就是选中，不是就是无效选择器
+  // ul li:last-of-type {}
+  // 先找到父元素，找到所有的类型为li的元素，选中最后一个
   &-item {
     a {
       color: #666;
@@ -51,6 +69,10 @@ export default {
     margin-left: 5px;
     margin-right: 5px;
     line-height: 22px;
+    // 样式的方式，不合理
+    // &:last-child {
+    //   display: none;
+    // }
   }
 }
 </style>
