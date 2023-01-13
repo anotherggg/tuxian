d3bc1c9d67e2fa3215f3bef6f4c60a534171d785
unknown <772172914@qq.com>
2023-01-13 16:56:07 +0800

    商品详情-基础布局

--- erabbit-pc-vue-project/src/router/index.js
+++ erabbit-pc-vue-project/src/router/index.js
@@ -5,12 +5,16 @@ import SubCategory from '@/views/category/sub';
 
 const Layout = () => import('@/views/Layout');
 const Home = () => import('@/views/home/index');
+const Goods = () => import('@/views/goods/index');
 
 const routes = [
   {
     path: '/',
     component: Layout,
-    children: [{ path: '/', component: Home }],
+    children: [
+      { path: '/', component: Home },
+      { path: '/product/:id', component: Goods },
+    ],
   },
   {
     path: '/category/:id',
